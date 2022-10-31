e51817d19db7c29e2bdc7d36ce2c207780a75207
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-10-31 20:59:40 +0200

    такс 8-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const creatCardContainer = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+creatCardContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+function createCardsMarkup(cards) {
+  return cards
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" 
+      href="${original}">
+      <img class="gallery__image"
+       src="${preview}" 
+       alt="${description}" />
+    </a>`;
+    }).join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt",captionDelay: 250 })
+
+  lightbox.show();
