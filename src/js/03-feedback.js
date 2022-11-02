dc7c49cc6a2e3f9f96fd5fe1df793318a80c75d1
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-11-02 11:36:15 +0200

    3 таск

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle'
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = "feedback-form-state";
 
+function populateTextarea() {
+    const serializedStats = localStorage.getItem(STORAGE_KEY);
+    const feedbackForm = JSON.parse(serializedStats) || {};
+    if (!feedbackForm) {
+        return;
+    }
+    const keys = Object.keys(feedbackForm);
+    console.log(keys);
+    for (const key of keys) {
+        form[key].value = feedbackForm[key];
+    }
+}
+function onTextareaInput({target}){
+    const serializedStats = localStorage.getItem(STORAGE_KEY);
+const addForm = JSON.parse(serializedStats) || {};
+addForm[target.name] = target.value;
+localStorage.setItem( STORAGE_KEY, JSON.stringify(addForm));
+console.log(serializedStats);
+}
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+}
+form.addEventListener('input', throttle(onTextareaInput, 500))
+form.addEventListener('submit', onFormSubmit);
+
+populateTextarea(form);
